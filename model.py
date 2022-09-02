94c6154b15c801f6db179ad12b7fa2d20924a902
user <ggmodhunters@gmail.com>
2022-09-02 18:09:20 +0300

    version 0.2 beta 2

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import pygame
 playeri1_1=pygame.Rect(1300/2,1000/2,100,100)
+backrooms_walls=pygame.Rect(400,500,400,300)
 fon1_1=pygame.Rect(0,0,1300,1000)
 poworot='none'
 moved = 1
--- view.py
+++ view.py
@@ -5,6 +5,9 @@ player1_1 = pygame.image.load('textures backrooms-infinity/player/player 1.png')
 player1_2 = pygame.image.load('textures backrooms-infinity/player/player 1-2.png')
 player1_3 = pygame.image.load('textures backrooms-infinity/player/player 1-3.png')
 player1_4 = pygame.transform.flip(player1_3,True,False)
+bakrooms_level_0_walls= pygame.image.load('textures backrooms-infinity/wals/wals_level_0.png')
+bakrooms_level_0_walls1 = pygame.Surface([100,100])#создаёт чёрную картинку размером 100 на 100
+bakrooms_level_0_walls1.blit(bakrooms_level_0_walls,[0,0])
 pygame.display.set_caption("backrooms-infinity")
 
 def scerd():
@@ -19,4 +22,5 @@ def scerd():
         screen.blit(player1_4, model.playeri1_1)
     if model.poworot == 'right':
         screen.blit(player1_3, model.playeri1_1)
+    screen.blit(bakrooms_level_0_walls1,model.backrooms_walls)
     pygame.display.flip()
